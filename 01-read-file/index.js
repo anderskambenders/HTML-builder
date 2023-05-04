a1e27d44832fdd673d88feb79d0b2dee1a2abee7
anderskambenders <a.zetsmanis@mail.ru>
2023-05-04 22:50:52 +0300

    fix: change path to file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path')
 
-const readableStream = fs.createReadStream(`${path.dirname(__filename)}/text.txt`, 'utf8');
+const readableStream = fs.createReadStream(path.join(path.dirname(__filename), 'text.txt'), 'utf8');
 let data = '';
 readableStream.on('data', (chunk) => {
   data += chunk;
